206a998b1579bd38c3e3616c75dc5559e6908b2c
yeah1900 <yeah1900@gmail.com>
2018-10-26 13:38:03 +0800

    rename project

--- SmartRename.js
+++ SmartRename.js
@@ -5,16 +5,18 @@ const ENV = {
     DRY_RUN_FLAG: process.env.DRY_RUN !== undefined ? process.env.DRY_RUN : true
 };
 
-const TARGET_PATTERN = new RegExp('^\\d{4}-\\d{2}-\\d{2} \\d{2}.\\d{2}.\\d{2}.(mp4|jpg|heic|png)$');
+const TARGET_PATTERN = new RegExp('^\\d{4}-\\d{2}-\\d{2} \\d{2}.\\d{2}.\\d{2}\.(mp4|jpg|heic|png)$');
 const FILE_MATCHERS = [
-    {type: 'regex', pattern: '^B612咔叽_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2}).(mp4|jpg)$'},
-    {type: 'regex', pattern: '^IMG_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2}).(mp4|jpg)$'},
-    {type: 'regex', pattern: '^VID_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2}).(mp4|jpg)$'},
-    {type: 'regex', pattern: '^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2})(\\d{2})(\\d{2}).(mp4|jpg|heic|png)$'},
-    {type: 'timestamp', pattern: '^mmexport(\\d{13}).(mp4|jpg|jpeg)$'},
-    {type: 'timestamp', pattern: '^microMsg.(\\d{13}).(mp4|jpg|jpeg)$'},
-    {type: 'timestamp', pattern: '^wx_camera_(\\d{13}).(mp4|jpg)$'},
-    {type: 'timestamp', pattern: '^(\\d{13}).(mp4|jpg)$'}
+    {type: 'regex', pattern: '^B612咔叽_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2})\.(mp4|jpg)$'},
+    {type: 'regex', pattern: '^IMG_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2})\.(mp4|jpg)$'},
+    {type: 'regex', pattern: '^VID_(\\d{4})(\\d{2})(\\d{2})_(\\d{2})(\\d{2})(\\d{2})\.(mp4|jpg)$'},
+    {type: 'regex', pattern: '^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2})(\\d{2})(\\d{2})\.(mp4|jpg|heic|png)$'},
+    {type: 'timestamp', pattern: '^mmexport(\\d{13})\.(mp4|jpg|jpeg)$'},
+    {type: 'timestamp', pattern: '^microMsg.(\\d{13})\.(mp4|jpg|jpeg)$'},
+    {type: 'timestamp', pattern: '^wx_camera_(\\d{13})\.(mp4|jpg)$'},
+    {type: 'timestamp', pattern: '^(\\d{13})\.(mp4|jpg)$'},
+    {type: 'birthtime', pattern: '^QQ视频_.{32}\.(mp4)$'},
+    {type: 'birthtime', pattern: '^null\-?.{15,16}\.(jpg)$'}
 ];
 
 const to2Digits = number => {
@@ -24,8 +26,9 @@ const to2Digits = number => {
     return number;
 };
 
-const normalize = name => {
+const normalize = (name, fullName) => {
     let normalizedName = null;
+
     FILE_MATCHERS.some(matcher => {
         if (matcher.type === 'regex') {
             const matchResult = new RegExp(matcher.pattern).exec(name);
@@ -50,9 +53,24 @@ const normalize = name => {
                 normalizedName = `${year}-${month}-${day} ${hour}.${min}.${sec}.${ext}`;
                 return true;
             }
+        } else if (matcher.type === 'birthtime') {
+            const matchResult = new RegExp(matcher.pattern).exec(name);
+
+            if (matchResult) {
+                const [,ext] = matchResult;
+                const createdDate = fs.statSync(fullName).birthtime;
+                const year = createdDate.getFullYear();
+                const month = to2Digits(createdDate.getMonth() + 1);
+                const day = to2Digits(createdDate.getDate());
+                const hour = to2Digits(createdDate.getHours());
+                const min = to2Digits(createdDate.getMinutes());
+                const sec = to2Digits(createdDate.getSeconds());
+                normalizedName = `${year}-${month}-${day} ${hour}.${min}.${sec}.${ext}`;
+            }
         }
         return false;
     });
+    
     return normalizedName;
 };
 
@@ -60,15 +78,15 @@ const renameFile = (folder, name, stats) => {
     stats.total++;
     if (!TARGET_PATTERN.test(name)) {
         const from = path.resolve(folder, name);
-        const normalizedName = normalize(name);
+        const normalizedName = normalize(name, from);
 
         if (normalizedName) {
-            const to = path.resolve(folder, normalize(name));
+            const to = path.resolve(folder, normalizedName);
             console.log('renaming ', from, ' to ', to);
             stats.renamed++;
             if (ENV.DRY_RUN_FLAG === 'false') {
-            	console.log('doing ', from, ' to ', to);
                 fs.renameSync(from, to);
+                console.log('rename completed');
             }
         } else {
             console.log('skipping ', name);
@@ -85,7 +103,7 @@ const renameFilesInFolder = folder => {
         unprocessed: 0
     };
     const files = fs.readdirSync(folder).toString().split(',');
-    files.forEach(file => {
+    files.filter(f => !!f).forEach(file => {
         const fileStats = fs.statSync(path.resolve(folder, file));
         if (fileStats.isFile()) {
             renameFile(folder, file, stats);
@@ -106,5 +124,6 @@ console.log(summary);
 
 // TODO:
 // 1. rollback feature - record name mappings
-// 2. logger
+// 2. replace console.log with nice logger
 // 3. support more file patterns
+// 4. birthtime
